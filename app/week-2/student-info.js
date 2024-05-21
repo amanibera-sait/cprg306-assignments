cb57bafefac1e0c195d7a9664971a36209a0da48
amanibera-sait <141461158+amanibera-sait@users.noreply.github.com>
2024-05-21 11:47:21 -0600

    Created the week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+export default function StudentInfo(){
+    return(
+    <div>
+        Amani Bera <br></br>
+        <a><link rel="stylesheet" href="https://github.com/amanibera-sait/cprg306-assignments"/></a>
+    </div>
+    );
+    
+   }
