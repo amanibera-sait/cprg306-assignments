f9b6325209c21b6226f20337b110ac3ebcb5dff6
amani56 <amani.bera@edu.sait.ca>
2024-07-02 22:30:33 -0600

    added useEffect to the shopping-list page to redirect non-authentiusers

--- app/week-8/shopping-list/page.js
+++ app/week-8/shopping-list/page.js
@@ -1,21 +1,28 @@
 "use client"
 
 
+import { useRouter } from "next/navigation";
 import { useUserAuth } from "../_utils/auth-context";
 import ItemList from "./item-list";
+import { useEffect } from "react";
+
 
 export default function Page() {
   const router = useRouter()
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  useEffect(()=>{if (!user){
+    redirectUser()
+  }},[])
   
   function redirectUser(){
-
+    router.push("/week-8")
   }
 
   return (
         <main>
           {!user && (
-            <div onLoad={redirectUser}>Redirecting</div>
+            <div onLoad={redirectUser} className="text-4xl font-bold p-6">Redirecting</div>
           )}
           {user && (
             <div>
